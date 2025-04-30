c64e5c11770340ff1bff7f9df09a5c577beb4150
ManyaZ1 <96390140+ManyaZ1@users.noreply.github.com>
2025-04-30 17:17:37 +0300

    organise the testing

--- region_growing.py
+++ region_growing.py
@@ -6,9 +6,9 @@ from skimage.draw import polygon2mask
 import matplotlib.pyplot as plt
 # Region growing ➔ GrabCut refinement ➔ Morphology ➔ Contour ➔ Convex Hull ➔ Midline Detection ➔ Splitting σε 2 Lanes ➔ Transparent fill
 # Load image
-path='C://Users//USER//Documents//_CAMERA_LIDAR//code//000005.png'
-path='C://Users//USER//Documents//_CAMERA_LIDAR//code//000015.png'
-#path='C://Users//USER//Documents//_CAMERA_LIDAR//code//004592.png'
+#path='C://Users//USER//Documents//_CAMERA_LIDAR//code//000005.png'
+#path='C://Users//USER//Documents//_CAMERA_LIDAR//code//000015.png'
+path='C://Users//USER//Documents//_CAMERA_LIDAR//code//004592.png'
 
 
 def region_growing(img, seeds, threshold)-> np.ndarray:
@@ -35,7 +35,7 @@ def region_growing(img, seeds, threshold)-> np.ndarray:
 
             for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]: #dfs
                 nx, ny = x+dx, y+dy # Neighbor coordinates
-
+                #horizon = int(h * 0.4) ; if 0 <= nx < w and horizon <= ny < h and visited[ny, nx] == 0:
                 if 0 <= nx < w and 0 <= ny < h and visited[ny, nx] == 0: # Check bounds and if not visited
                     neighbor_value = img[ny, nx] # Grayscale value of neighbor
                     visited[ny, nx] = 1
@@ -61,21 +61,13 @@ def grab_cut(img, initial_mask, iterations=5)-> np.ndarray:
     """
     h, w = initial_mask.shape
 
-    # Initialize GrabCut mask
-    #grabcut_mask = np.full((h, w), cv2.GC_PR_BGD, dtype=np.uint8)  # Probable background by default
-    #grabcut_mask[initial_mask == 255] = cv2.GC_PR_FGD  # Probable foreground from region growing
-    grabcut_mask = np.full((h, w), cv2.GC_PR_BGD, dtype=np.uint8)  # default probable background
-    grabcut_mask[road_mask == 255] = cv2.GC_PR_FGD
+    # # Initialize GrabCut mask
+    #grabcut_mask = build_grabcut_mask_from_road_mask_old(initial_mask)  # default probable background
 
-    # ενισχυμένα σίγουρα foreground (στο κέντρο μόνο)
-    #horizon=int(h*0.4)
-    horizon=0
-    center_strip = road_mask[horizon:, w//3:2*w//3]
-    grabcut_mask[:, w//3:2*w//3][center_strip == 255] = cv2.GC_FGD
-
-    # Models (empty, OpenCV needs them)
+    # # # Models (empty, OpenCV needs them)
     bgdModel = np.zeros((1, 65), np.float64)
     fgdModel = np.zeros((1, 65), np.float64)
+    grabcut_mask = build_grabcut_mask_from_road_mask(initial_mask, strong_foreground_width_ratio=0.3)
 
     # Apply GrabCut
     cv2.grabCut(img, grabcut_mask, None, bgdModel, fgdModel, iterations, cv2.GC_INIT_WITH_MASK)
@@ -88,9 +80,10 @@ def grab_cut(img, initial_mask, iterations=5)-> np.ndarray:
 
     return final_mask
 
+#helper function to convert road mask to grabcut mask
 def build_grabcut_mask_from_road_mask(road_mask: np.ndarray, strong_foreground_width_ratio=0.3) -> np.ndarray:
     """
-    Converts a binary road_mask into a proper grabcut mask:
+    Converts a binary road_mask into grabcut mask:
     - center region becomes sure foreground (GC_FGD)
     - rest of white region becomes probable foreground (GC_PR_FGD)
     - black remains probable background (GC_PR_BGD)
@@ -116,32 +109,106 @@ def build_grabcut_mask_from_road_mask(road_mask: np.ndarray, strong_foreground_w
 
     return grabcut_mask
 
+def build_grabcut_mask_from_road_mask_old(initial_mask: np.ndarray) -> np.ndarray:
+    road_mask=initial_mask.copy()
+    h, w = initial_mask.shape
+    grabcut_mask = np.full((h, w), cv2.GC_PR_BGD, dtype=np.uint8)  # default probable background
+    grabcut_mask[road_mask == 255] = cv2.GC_PR_FGD
+    # ενισχυμένα σίγουρα foreground (στο κέντρο μόνο)
+    horizon=int(h*0.4)
+    horizon=0
+    center_strip = road_mask[horizon:, w//3:2*w//3]
+    grabcut_mask[:, w//3:2*w//3][center_strip == 255] = cv2.GC_FGD
+    return grabcut_mask
+
+def image_loader(root_path, image_name):
+    'path->return image'
+    image_path = root_path + image_name
+    image = cv2.imread(image_path)
+    # Check if the image was loaded successfully
+    if image is None:
+        print("Error loading image")
+        exit(1)
+    return image
+
+def post_processingv1(road_mask: np.ndarray) -> np.ndarray:
+    # Post-processing v1
+    #close shape
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+    road_mask = cv2.morphologyEx(road_mask, cv2.MORPH_CLOSE, kernel)
+    # Then dilate separately (to expand)
+    dilation_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5)) #SOS VERY IMPORTANT
+    road_mask = cv2.dilate(road_mask, dilation_kernel, iterations=2)
+    return road_mask
+
+def detect_road(img):
+
+    #Grayscale image
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Define seeds (e.g., bottom center area)
+    h, w = gray.shape
+    seeds = [(w//2, int(h*0.95))]  # one seed at bottom center
+
+    # Run region growing 
+    road_mask = region_growing(gray, seeds, threshold=5)
+
+    # Post-processing v1
+    road_mask = post_processingv1(road_mask)
+    # GrabCut refinement
+    road_mask_grabcut = grab_cut(img, road_mask,iterations=15)
+
+    cv2.imshow('GrabCut Refined Road', road_mask_grabcut)
+    #cv2.imshow('Detected Road by region growing', road_mask)
+    cv2.imshow('Original Image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
-# Load image
-img = cv2.imread(path)
-#grayscale image
-#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(img, (5,5), 0)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-gray_clahe = clahe.apply(gray)
-# Define seeds (e.g., bottom center area)
-h, w = gray.shape
-seeds = [(w//2, int(h*0.95))]  # one seed at bottom center
+if __name__ == "__main__":
+    import os
+#     names=["000015.png","007121.png","000005.png","004592.png","um_000002.png"]
+#    #names=["000005.png"]
+#     for name in names:
+#         image=image_loader(root_path="C:/Users/USER/Documents/_CAMERA_LIDAR/code/", image_name=name)
+#         detect_lanes(image)
+    
+    input_folder = r'C:/Users/USER/Documents/_CAMERA_LIDAR/code/training/image_2/'  # π.χ. './images'
+    for filename in os.listdir(input_folder):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            img_path = os.path.join(input_folder, filename)
+            image = cv2.imread(img_path)
 
-# Run region growing
-threshold = 5  # Try different values
-road_mask = region_growing(gray, seeds, threshold)
+            if image is None:
+                print(f"Could not load image {filename}")
+                continue
+            detect_road(image)
 
 
 
-# Post-processing v1
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
-road_mask = cv2.morphologyEx(road_mask, cv2.MORPH_CLOSE, kernel)
-# Then dilate separately (to expand)
-dilation_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
-road_mask = cv2.dilate(road_mask, dilation_kernel, iterations=2)
+# # Load image
+# #img = cv2.imread(path)
+# #grayscale image
+# #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# blurred = cv2.GaussianBlur(img, (5,5), 0)
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+# gray_clahe = clahe.apply(gray)
+# # Define seeds (e.g., bottom center area)
+# h, w = gray.shape
+# seeds = [(w//2, int(h*0.95))]  # one seed at bottom center
+
+# # Run region growing
+# threshold = 5  # Try different values
+# road_mask = region_growing(gray, seeds, threshold)
+
+
+
+# # Post-processing v1
+# kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+# road_mask = cv2.morphologyEx(road_mask, cv2.MORPH_CLOSE, kernel)
+# # Then dilate separately (to expand)
+# dilation_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5)) #SOS VERY IMPORTANT
+# road_mask = cv2.dilate(road_mask, dilation_kernel, iterations=2)
 
 # Post-processing v2
 # kernel = np.ones((5, 5), np.uint8)
@@ -159,37 +226,11 @@ road_mask = cv2.dilate(road_mask, dilation_kernel, iterations=2)
 # After road_mask is computed
 
 
-## GrabCut refinement
-road_mask_refined = grab_cut(img, road_mask,iterations=5)
+# ## GrabCut refinement
+# road_mask_grabcut = grab_cut(img, road_mask,iterations=15)
 
-cv2.imshow('GrabCut Refined Road', road_mask_refined)
-
-# Post-processing: Morphology
-contours, _ = cv2.findContours(road_mask_refined, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-if not contours:
-    raise ValueError("No contours found in the mask.")
-largest_contour = max(contours, key=cv2.contourArea)
-filtered_mask = np.zeros_like(road_mask)
-cv2.drawContours(filtered_mask, [largest_contour], -1, 255, thickness=cv2.FILLED)
-
-# Step 2: Skeletonize (get midline)
-skeleton = skeletonize((filtered_mask > 0).astype(np.uint8))
-
-# Step 3: Expand a fixed-width region around the skeleton (e.g., width 20 px)
-skeleton_coords = np.column_stack(np.where(skeleton > 0))
-dilated_skeleton = np.zeros_like(skeleton, dtype=np.uint8)
-for y, x in skeleton_coords:
-    cv2.circle(dilated_skeleton, (x, y), radius=10, color=1, thickness=-1)
-
-# Step 4: Apply the new refined mask
-refined_road_mask = (dilated_skeleton * 255).astype(np.uint8)
-
-
-cv2.imshow('Morphology Refined Road', road_mask_refined)
-
-
-#cv2.imshow('Detected Road by region growing', road_mask)
-cv2.imshow('Original Image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+# cv2.imshow('GrabCut Refined Road', road_mask_grabcut)
+# #cv2.imshow('Detected Road by region growing', road_mask)
+# cv2.imshow('Original Image', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
